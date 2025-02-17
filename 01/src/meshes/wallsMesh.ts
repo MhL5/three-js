51e1ae44a39372a5fa605b1d1d0336e0d91cf0b5
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-02-17 16:16:34 +0330

    add wall mesh textures

--- 01/src/meshes/wallsMesh.ts
+++ 01/src/meshes/wallsMesh.ts
@@ -1,9 +1,25 @@
 import * as THREE from "three";
+import {
+  bricksAmbientOcclusionTexture,
+  bricksColorTexture,
+  bricksNormalTexture,
+  bricksRoughnessTexture,
+} from "../TextureLoader";
 
 const wallsWHD = [4, 2.5, 4];
 const walls = new THREE.Mesh(
   new THREE.BoxGeometry(...wallsWHD),
-  new THREE.MeshStandardMaterial({ color: "#ac8e82" })
+  new THREE.MeshStandardMaterial({
+    map: bricksColorTexture,
+    aoMap: bricksAmbientOcclusionTexture,
+    normalMap: bricksNormalTexture,
+    roughnessMap: bricksRoughnessTexture,
+  })
+);
+// aoMap
+walls.geometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(walls.geometry.attributes.uv.array, 2)
 );
 walls.position.y = wallsWHD[1] / 2;
 
