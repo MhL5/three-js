b50f7c884726df66f1729f4ab94efecff641aafa
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-02-17 16:35:10 +0330

    add grass

--- 01/src/meshes/floorMesh.ts
+++ 01/src/meshes/floorMesh.ts
@@ -1,10 +1,38 @@
 import * as THREE from "three";
+import {
+  grassAmbientOcclusionTexture,
+  grassColorTexture,
+  grassNormalTexture,
+  grassRoughnessTexture,
+} from "../TextureLoader";
+import { setUV2Attribute } from "../utils/setUV2Attribute";
+
+grassColorTexture.repeat.set(8, 8);
+grassAmbientOcclusionTexture.repeat.set(8, 8);
+grassNormalTexture.repeat.set(8, 8);
+grassRoughnessTexture.repeat.set(8, 8);
+
+grassColorTexture.wrapS = THREE.RepeatWrapping;
+grassAmbientOcclusionTexture.wrapS = THREE.RepeatWrapping;
+grassNormalTexture.wrapS = THREE.RepeatWrapping;
+grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
+
+grassColorTexture.wrapT = THREE.RepeatWrapping;
+grassAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+grassNormalTexture.wrapT = THREE.RepeatWrapping;
+grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
 
 const floorWH = [20, 20];
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(...floorWH),
-  new THREE.MeshStandardMaterial({ color: "#a9c388" })
+  new THREE.MeshStandardMaterial({
+    map: grassColorTexture,
+    aoMap: grassAmbientOcclusionTexture,
+    normalMap: grassNormalTexture,
+    roughnessMap: grassRoughnessTexture,
+  })
 );
+setUV2Attribute(floor);
 floor.rotation.x = -Math.PI * 0.5;
 floor.position.y = 0;
 
